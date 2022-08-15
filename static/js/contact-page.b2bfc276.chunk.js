040be5dc36c204b6c8a577cd2723d925443a34ae
DmitriyGovosyants <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-08-15 10:40:48 +0000

    Deploying to gh-pages from @ DmitriyGovosyants/goit-react-hw-08-phonebook@ee12a84a54302ecd3029fa88cb49854924f2565b 🚀

--- static/js/contact-page.b2bfc276.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[186],{2910:function(e,t,n){n.r(t);var r=n(134),s=n(3329);t.default=function(){return(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)(r.U3,{children:"Contacts"}),(0,s.jsx)(r.wn,{}),(0,s.jsx)(r.t,{}),(0,s.jsx)(r.CR,{})]})}}}]);
-//# sourceMappingURL=contact-page.b2bfc276.chunk.js.map
